6d21702e1557c7dc3a42202af7c96e63dcf04fcb
James Smith <jim@codejim.com>
2025-11-22 09:54:45 -0500

    hellow mow

---
+++ mow-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mow-ios
+//
+//  Created by James Smith on 11/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mow-ios/MowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MowApp.swift
+//  mow-ios
+//
+//  Created by James Smith on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
